a7a1f4c4d5b7e3832280b0c871391e7e8d7119d4
JFManiac <jfmaniac@gmail.com>
2017-05-26 15:44:28 -0500

    Added connectionCache class, use to check if SQLite connection is open/existing. If not add and open. To keep from opening the same connection over and over again.

---
+++ SampleClass/connectionCache.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Data;
+
+namespace SampleClass
+{
+    public class connectionCache
+    {
+        // Define variables
+        Boolean ConnectionsInitialized = false;
+        string[] ConnectionsConnectionString;
+        SQLiteConnection[] ConnectionsDataConnection;
+        DateTime[] ConnectionsTimeStamp;
+
+        public SQLiteConnection GetDataConnectionFromCache(ref string ConnectionString)
+        {
+
+            if (ConnectionsInitialized = false)
+            {
+                // Initialize variables
+                string[] ConnectionsConnectionString = new string[50];
+                SQLiteConnection[] ConnectionsDataConnection = new SQLiteConnection[50];
+                DateTime[] ConnectionsTimeStamp = new DateTime[50];
+                ConnectionsInitialized = true;
+
+                int Oldest = 0;
+                DateTime OldestTime;
+
+                //   See if connection already in cache
+                for (int i = 0; i < ConnectionsConnectionString.Length; i++)
+                {
+                    if (ConnectionsConnectionString[i] == ConnectionString)
+                    {
+                        ConnectionsTimeStamp[i] = DateTime.Now;
+
+                        //Must check to see if connection still valid and open. If it isn't, open it here
+
+                        if (ConnectionsDataConnection[i].State == ConnectionState.Closed)
+                        {
+
+                            try
+                            {
+                                ConnectionsDataConnection[i].ConnectionString = ConnectionsConnectionString[i];
+                                ConnectionsDataConnection[i].Open();
+                            }
+                            catch (Exception ex)
+                            {
+                                throw new Exception("Connecting to SQL failed" + ConnectionString + "  " + ex.Message + "  " + ex.StackTrace);
+                                return null;
+                            }
+
+                            return ConnectionsDataConnection[i];
+                        }
+                    }
+                }
+
+                //   Add new connection if not in the list
+                for (int i = 0; i < ConnectionsConnectionString.Length; i++)
+                {
+                    if (ConnectionsConnectionString[i] == "")
+                    {
+
+                        ConnectionsConnectionString[i] = ConnectionString;
+                        ConnectionsDataConnection[i] = new SQLiteConnection();
+                        try
+                        {
+                            ConnectionsDataConnection[i].ConnectionString = ConnectionsConnectionString[i];
+                            ConnectionsDataConnection[i].Open();
+                        }
+                        catch (Exception ex)
+                        {
+
+                            throw new Exception("Connecting to SQL failed" + ConnectionString + "  " + ex.Message + "  " + ex.StackTrace);
+                            return null;
+                        }
+
+                        ConnectionsTimeStamp[i] = DateTime.Now;
+                        return ConnectionsDataConnection[i];
+                    }
+                }
+
+                OldestTime = DateTime.Now;
+                //  Now find the oldest connection and reuse that one
+                //  Note: if this fails, the code will reuse the connection in index 0. That is ok.
+                for (int i = 0; i < ConnectionsConnectionString.Length; i++)
+                {
+                    if (ConnectionsTimeStamp[i] < OldestTime)
+                    {
+                        Oldest = i;
+                        OldestTime = ConnectionsTimeStamp[i];
+                    }
+                }
+
+                //   Close connection in index Oldest if it is open
+                if (ConnectionsDataConnection[Oldest].State == ConnectionState.Open)
+                {
+                    ConnectionsDataConnection[Oldest].Close();
+                }
+
+                ConnectionsConnectionString[Oldest] = ConnectionString;
+
+                try
+                {
+                    ConnectionsDataConnection[Oldest].ConnectionString = ConnectionString;
+                    ConnectionsDataConnection[Oldest].Open();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Connecting to SQL failed" + ConnectionString + "  " + ex.Message + "  " + ex.StackTrace);
+                    return null;
+                }
+
+                ConnectionsTimeStamp[Oldest] = DateTime.Now;
+                return ConnectionsDataConnection[Oldest];
+            }
+
+            return null;
+        }
+
+        public connectionCache()
+        {
+            // Constructor
+
+
+        }
+    }
+}
---
+++ SampleClass/dbMethods.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace SampleClass1
+{
+    public class dbMethods
+    {
+        //
+
+        public static Boolean dbCreate(ref string dbConnection, ref string dbName)
+        {
+            SQLiteConnection dbConn = new SQLiteConnection();
+
+            if (dbConnection == "")
+            {
+                throw new Exception("No connection!");
+                return false;
+            }
+
+            return dbCreate(ref GetDataConnectionFromCache(dbConnection), ref dbName);
+        }
+
+        public static Boolean dbCreate(ref SQLiteConnection dbConnection, ref string dbName)
+        {
+            try
+            {
+                SQLiteConnection.CreateFile("Contacts.db");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Create database failed!", ex);
+                return false;
+            }
+
+            dbConnection = new SQLiteConnection("Data Source=Contacts.db;Version=3;");
+            dbConnection.Open();
+
+            String sqlString = "create table names (name varchar(30), age int)";
+
+            SQLiteCommand command = new SQLiteCommand(sqlString, dbConnection);
+
+            try
+            {
+                command.ExecuteNonQuery();
+            }
+            catch
+            {
+                throw new Exception("Execute failed!", ex);
+                return false;
+            }
+
+            return true;
+
+        }
+
+        public static void dbInsert()
+        {
+
+        }
+
+    }
+
+
+
+}
