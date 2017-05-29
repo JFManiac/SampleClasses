b62cf9350d597eeb5ff1f7f3b75b701455267ddc
JF <JF@BORR>
2017-05-28 22:15:53 -0500

    Revamped a few things, SampleConsole opens or creates the database of your choice.

--- SampleClass/dbMethods.cs
+++ SampleClass/dbMethods.cs
@@ -11,35 +11,36 @@ namespace SampleClass1
     {
         //
         
-        public static Boolean dbCreate(ref string dbConnection, ref string dbName)
+        //public static SQLiteConnection dbCreate(ref string dbName)
+        //{
+            //connectionCache dbCache = new connectionCache();
+            //SQLiteConnection dbConn = new SQLiteConnection();
+
+            //if (dbConnection == "")
+            //{
+            //    throw new Exception("No connection!");
+            //    return null;
+            //}
+
+            //dbConn = dbCache.GetDataConnectionFromCache(ref dbConnection);
+
+           // return dbCreate(ref dbName);
+        //}
+
+        public static SQLiteConnection dbCreate(ref string dbName)
         {
-            connectionCache dbCache = new connectionCache();
-            SQLiteConnection dbConn = new SQLiteConnection();
-
-            if (dbConnection == "")
-            {
-                throw new Exception("No connection!");
-                return false;
-            }
-
-            dbConn = dbCache.GetDataConnectionFromCache(ref dbConnection);
-
-            return dbCreate(ref dbConn, ref dbName);
-        }
-
-        public static Boolean dbCreate(ref SQLiteConnection dbConnection, ref string dbName)
-        {
+            SQLiteConnection dbConnection;
             try
             {
-                SQLiteConnection.CreateFile("Contacts.db");
+                SQLiteConnection.CreateFile(dbName);
             }
             catch (Exception ex)
             {
                 throw new Exception("Create database failed!", ex);
-                return false;
+                return null;
             }
 
-            dbConnection = new SQLiteConnection("Data Source=Contacts.db;Version=3;");
+            dbConnection = new SQLiteConnection("Data Source=" + dbName + ";Version=3;");
             dbConnection.Open();
 
             String sqlString = "create table names (name varchar(30), age int)";
@@ -53,15 +54,21 @@ namespace SampleClass1
             catch (Exception ex)
             {
                 throw new Exception("Execute failed!", ex);
-                return false;
+                return null;
             }
 
-            return true;
+            return dbConnection;
 
         }
 
         public static void dbInsert()
         {
+
+        }
+
+       public dbMethods()
+        {
+            // Constructor
 
         }
 
--- SampleConsole/Program.cs
+++ SampleConsole/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SampleClass1;
+using System.Data.SQLite;
 
 namespace SampleConsole
 {
@@ -11,7 +12,23 @@ namespace SampleConsole
     {
         static void Main(string[] args)
         {
+    
+            Console.WriteLine("Opening dbConnection and creating database.");
+            string dbName = Console.ReadLine();
+
+            try
+            {
+                SQLiteConnection dbConnection = dbMethods.dbCreate(ref dbName);
+                Console.WriteLine("Success.");
+            }
             
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed!");
+                Console.WriteLine(ex.Message + "  " + ex.StackTrace);
+                Console.ReadLine();
+                
+            }
         }
     }
 }
